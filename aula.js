cd630d8d61f4f46632765526a975662c12144255
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-15 09:36:32 -0300

    add js

---
+++ aula.js
@@ -0,0 +1,14 @@
+//aqui e um alert 
+
+/*
+aprendendo java script
+ muito sono 
+*/
+alert("ola mundo")
+
+document.write("meu nome e guilherme");
+//usando o console
+console.log("chatao ");
+//criando variavel 
+let mensagem ="ola mundo";
+console.log("esse console e da variavel ", mensagem);
