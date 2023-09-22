39c67f1275c2dd07006273282c06c14f35c34903
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-22 09:37:36 -0300

    esse e um texto modificado

--- operadores.js
+++ operadores.js
@@ -1,7 +1,14 @@
 //declaraçao de variavel 
 let num1 = 10;
 let num2 = 5;
+
 //operaçoes matematicas 
 let soma = num1 + num2;
+let subtracao = num1 - num2;
+let divisao = num1 / num2;
+let multiplicacao = num1 * num2
 // exbindo o resultado no console 
 console.log("o resultado da soma é ", soma)
+console.log("a o resulado da subtracao e ", subtracao )
+console.log("divisao e ", divisao)
+console.log("multiplicação e  ", multiplicacao)
