cf9d8bfe90de22075da2bfe2371ff326101daab5
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-22 09:15:13 -0300

    add texto

---
+++ texto-numero.js
@@ -0,0 +1,13 @@
+let nunFloat = 123.5422;
+console.log(parseInt(nunFloat))
+let nunString = "123.5432";
+console.log(typeof nunString)
+console.log(parseFloat(nunString))
+//
+let nomeAluno1= "pedro henrique";
+console.log(nomeAluno1.length);
+
+let txt = "estao chegando as provas"
+console.log(txt.indexOf("as"));
+console.log(txt.slice(0,5));
+console.log(txt.substr(6,8));
