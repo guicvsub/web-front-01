7340157c4908160b6aada178b1cf5017395519e7
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-22 09:35:49 -0300

    add o operadores

---
+++ manipulando-texto.js
@@ -0,0 +1,7 @@
+let txt = "estao chegando as provas";
+let num = 123.5432;
+
+//console.log(txt.replace("provas", "avaliaçoes"));
+//letra maiuscula 
+console.log(txt.toUpperCase());
+console.log(num.toFixed(2));
---
+++ operadores.js
@@ -0,0 +1,7 @@
+//declaraçao de variavel 
+let num1 = 10;
+let num2 = 5;
+//operaçoes matematicas 
+let soma = num1 + num2;
+// exbindo o resultado no console 
+console.log("o resultado da soma é ", soma)
