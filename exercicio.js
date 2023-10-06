113439e96a666ee5f68a384245cb53f7e1ea12c0
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-06 09:42:21 -0300

    esse e o commit bomba

---
+++ exercicio.js
@@ -0,0 +1,16 @@
+let frutas = ["morango", "banana", "pesego", ];
+
+if(frutas.includes("abacaxi")){
+    console.log("a sring abacaxi existe no array")
+}
+
+else if(frutas.includes("pera")){
+     console.log("pera existe no array frutas")
+}
+else{
+
+    console.log("nem pera nen abacaxi existe no aray frutas")
+}
+    
+
+
---
+++ switch.js
@@ -0,0 +1,12 @@
+let teste = 2 
+switch(teste){
+    case 1:
+        console.log("foi de primeira" );
+        break
+    case 2:
+        console.log("foi de segunda");
+        break;
+    default:
+        console.log("nao foi na primeira nem na segunda ")      
+
+}
