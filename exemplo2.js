c789225bdd1bf826784ae0845d676474ca675553
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-20 08:47:50 -0300

    add exemplo de decremento

---
+++ exemplo2.js
@@ -0,0 +1,10 @@
+let valor = 7 
+console.log(valor)
+valor--;
+console.log(valor)
+valor-=2
+console.log(valor)
+valor*=2 
+console.log("multiplicaçaão ", valor )
+valor /=2;
+console.log("divisao", valor)
