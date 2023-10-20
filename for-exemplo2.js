7b944bb71577a2c43f3e74d1416d8ee6a6aa59fa
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-20 09:13:05 -0300

    adicionando for

---
+++ for-exemplo2.js
@@ -0,0 +1,5 @@
+let fruta = ["maca","banana","uva","pera"];
+console.log(fruta.length);
+for(let i =0; i <fruta.length; i++) {
+ console.log(`-${fruta[i]});
+}
---
+++ for-exemplo3.js
@@ -0,0 +1,4 @@
+let frutas = ["maca","banana","uva","pera"];
+for (let frutas of frutas){
+    console.log(frutas);
+}
