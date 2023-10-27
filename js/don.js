4c1965ee553fbfe34ee7832c8b6faa355a0a3a8d
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-27 08:49:56 -0300

    esse e o comit de função

---
+++ js/don.js
@@ -0,0 +1,11 @@
+function teste(arg1,arg2) {
+    alert("minha primeira função")
+    console.log(arg1 + arg2)
+}
+teste(7, 1);
+
+function ImprimeNome(name){
+    console.log(`meu nome e ${name}`);
+}
+
+ImprimeNome("geovani")
